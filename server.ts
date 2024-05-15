afef997d4c873fa2cd6c31db590ca05b55b8f476
Mohd Arsalan <thesiddiqui7@gmail.com>
2024-05-15 13:02:02 +0530

    Change default server port from 10000 to 3000

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 // const PORT = process.env.PORT1 || process.env.PORT2 || process.env.PORT3 || 18012 || 18012 || 19099;
 // const PORT = 8081;
 
-const PORT = 10000;
+const PORT = 3000;
 
 connectDB();
 
