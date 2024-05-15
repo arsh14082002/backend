9e5bfec6f6029589237ac9bc74f99572cd396bc2
Mohd Arsalan <thesiddiqui7@gmail.com>
2024-05-15 12:00:30 +0530

    "Updated PORT configuration to include additional options"

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import app from './src/app';
 import { config } from './src/config/config';
 import connectDB from './src/config/db';
 
-const PORT = config.port1 || config.port2 || config.port3 || 5500;
+const PORT = config.port1 || config.port2 || config.port3 || 18012 || 18013 || 19099;
 // const PORT = 8081;
 
 connectDB();
