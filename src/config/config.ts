0e851ce34b71eb318179c7b99eab4b60d7564046
Mohd Arsalan <thesiddiqui7@gmail.com>
2024-05-15 11:54:22 +0530

    Update server port configuration and server.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,9 @@ import { config as conf } from 'dotenv';
 conf();
 
 const _config = {
-  port: process.env.PORT,
+  port1: process.env.PORT1,
+  port2: process.env.PORT2,
+  port3: process.env.PORT3,
   mongoURI: process.env.MONGODB_URI,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECERET,
